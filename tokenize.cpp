e19d561b0c11abce5165af7e078c523a28f7266a
awi29 <das.awanish@gmail.com>
2016-12-01 17:47:09 +0530

    Add files via upload

---
+++ tokenize.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+
+int main()
+	{
+		fstream fp("outputf.in") ;
+		string line = "" ;
+
+		 while( getline( fp , line) )
+		 		{
+		 			vector<string> v;
+                   
+                   int i = 0;
+                    stringstream ssin(line);
+                  while (ssin.good() )
+                  {
+                  ssin >> s[i];
+                   ++i;
+                 }
+               
+               }
+
+
+		 		
+
+
+	return 0 ;
+	}
+ 
